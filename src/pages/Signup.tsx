3277ce1d5899b2b65959d0142e7aae1a07b0f4e2
khantnandeesh <148197396+khantnandeesh@users.noreply.github.com>
2024-08-21 18:54:13 +0530

    Update Signup.tsx

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -54,7 +54,7 @@ const GarbaCommitteeForm = () => {
               className="w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
               type="text"
               id="registrationNumber"
-              name="registrationNumber"
+              name="regestrationNumber"
               value={formData.regestrationNumber}
               onChange={handleChange}
               required
